3b414e0ea3858d0872d0c6179bb52db37760d5d1
Kevin Persaud <kevinpersaud@Kevins-MacBook-Pro.local>
2023-07-04 20:52:17 -0400

    Added Google Button

--- src/components/LoginComponent.tsx
+++ src/components/LoginComponent.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { LoginAPI } from '../api/AuthAPI'
+import GoogleButton from 'react-google-button'
 import '../SCSS/LoginComponent.scss'
 
 export default function LoginComponent() {
@@ -18,22 +19,35 @@ export default function LoginComponent() {
     
   }
   return (
-    <div className="login-wrapper">
-      <h1>LoginComponent</h1>
-      <div className="auth-inputs">
-        <input
-          onChange={(event) => setCredentials({... credentials, email: event.target.value})}
-          className="common-input"
-          placeholder='Enter your email'
-        />
-        <input
-          onChange={(event) => setCredentials({... credentials, password: event.target.value})}
-          className="common-input"
-          placeholder='Enter your password'
-        />
-      </div>
-      
-      <button onClick={login} className="login-btn">Log In to Co-Lab</button>
-    </div>
+    <React.Fragment>
+      <h1 className="colab-logo">Co-Lab</h1>      
+      <div className="login-wrapper">
+        <h2 className="sign-in-header sign-in-text">Sign In</h2>
+        <p className="sign-in-subtext sign-in-text">Start Collaborating Now</p>
+        <div className="auth-inputs">
+          <input
+            onChange={(event) => setCredentials({... credentials, email: event.target.value})}
+            className="common-input"
+            placeholder='Enter your email'
+            type="email"
+          />
+          <input
+            onChange={(event) => setCredentials({... credentials, password: event.target.value})}
+            className="common-input"
+            placeholder='Enter your password'
+            type="password"
+          />
+        </div>
+        <button onClick={login} className="login-btn">Log In to Co-Lab</button>
+        <hr className="hr-text" data-content="OR"></hr>
+        <div className="google-btn-container">
+          <GoogleButton
+            className="google-btn"
+          />
+          <p>New to CoLab? <span className="join-now">Join now</span></p>
+        </div>
+      </div>   
+    </React.Fragment>
+    
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import {
 } from "react-router-dom";
 import { router } from './Routes/index.tsx';
 import { app } from '../src/firebaseConfig.ts'
-import '/src/css/index.css'
+import '/src/SCSS/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
